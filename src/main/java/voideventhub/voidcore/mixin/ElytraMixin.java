0a900e59037cce6e5b469a832a01bbe6aff6c125
DaanSchar <Daan.Schaer@Gmail.com>
2023-08-10 16:50:50 +0200

    feat: added elytra and handheld render mixins

---
+++ src/main/java/voideventhub/voidcore/mixin/ElytraMixin.java
@@ -0,0 +1,58 @@
+package voideventhub.voidcore.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.PlayerModelPart;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.ElytraEntityModel;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Environment(EnvType.CLIENT)
+@Mixin(value = ElytraFeatureRenderer.class, priority = 650)
+public abstract class ElytraMixin <T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
+
+
+    @Shadow @Final
+    private static Identifier SKIN;
+
+    @Shadow @Final
+    private ElytraEntityModel<T> elytra;
+
+    public ElytraMixin(FeatureRendererContext<T, M> context) {
+        super(context);
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
+        AbstractClientPlayerEntity abstractClientPlayerEntity;
+        ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.CHEST);
+//        if (!itemStack.isOf(Items.ELYTRA)) {
+//            return;
+//        }
+        Identifier identifier = livingEntity instanceof AbstractClientPlayerEntity ? ((abstractClientPlayerEntity = (AbstractClientPlayerEntity)livingEntity).canRenderElytraTexture() && abstractClientPlayerEntity.getElytraTexture() != null ? abstractClientPlayerEntity.getElytraTexture() : (abstractClientPlayerEntity.canRenderCapeTexture() && abstractClientPlayerEntity.getCapeTexture() != null && abstractClientPlayerEntity.isPartVisible(PlayerModelPart.CAPE) ? abstractClientPlayerEntity.getCapeTexture() : SKIN)) : SKIN;
+        matrixStack.push();
+        matrixStack.translate(0.0, 0.0, 0.125);
+        this.getContextModel().copyStateTo(this.elytra);
+        this.elytra.setAngles(livingEntity, f, g, j, k, l);
+        VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(identifier), false, itemStack.hasGlint());
+        this.elytra.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, 1.0f);
+        matrixStack.pop();
+    }
+
+}
---
+++ src/main/java/voideventhub/voidcore/mixin/HeldItemMixin.java
@@ -0,0 +1,48 @@
+package voideventhub.voidcore.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Arm;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+//TODO: There is currently a bug where the actual held item is rendered in first person. Third person looks fine.
+@Mixin(value = HeldItemFeatureRenderer.class, priority = 650)
+public abstract class HeldItemMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
+
+    @Shadow
+    protected void renderItem(LivingEntity entity, ItemStack stack, ModelTransformation.Mode transformationMode, Arm arm, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+    }
+
+    public HeldItemMixin(FeatureRendererContext<T, M> context) {
+        super(context);
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
+        boolean bl = livingEntity.getMainArm() == Arm.RIGHT;
+        ItemStack itemStack = bl ? livingEntity.getOffHandStack() : livingEntity.getMainHandStack();
+        ItemStack itemStack2 = bl ? livingEntity.getMainHandStack() : livingEntity.getOffHandStack();
+        if (!itemStack.isEmpty() || !itemStack2.isEmpty()) {
+            matrixStack.push();
+            if (this.getContextModel().child) {
+                float m = 0.5F;
+                matrixStack.translate(0.0, 0.75, 0.0);
+                matrixStack.scale(0.5F, 0.5F, 0.5F);
+            }
+
+            this.renderItem(livingEntity, Items.SHIELD.getDefaultStack(), ModelTransformation.Mode.THIRD_PERSON_RIGHT_HAND, Arm.RIGHT, matrixStack, vertexConsumerProvider, i);
+            this.renderItem(livingEntity, Items.DIAMOND_AXE.getDefaultStack(), ModelTransformation.Mode.THIRD_PERSON_LEFT_HAND, Arm.LEFT, matrixStack, vertexConsumerProvider, i);
+            matrixStack.pop();
+        }
+    }
+
+}
