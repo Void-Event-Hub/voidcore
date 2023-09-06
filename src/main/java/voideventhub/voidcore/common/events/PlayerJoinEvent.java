9d47aeb08d9c55477f87358058705296ce1eee58
DaanSchar <Daan.Schaer@Gmail.com>
2023-09-06 09:13:45 +0200

    fix: changed EntityLoadEvent to PlayerJoinEvent entityLoad was also called whenever a player respawned. we only want the join server behaviour.

---
+++ src/main/java/voideventhub/voidcore/common/events/PlayerJoinEvent.java
@@ -0,0 +1,24 @@
+package voideventhub.voidcore.common.events;
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import voideventhub.voidcore.common.PlayerActionService;
+import voideventhub.voidcore.common.VoidCore;
+
+public class PlayerJoinEvent {
+
+    public static void register() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            if (server.isSingleplayer() ||
+                    VoidCore.CONFIG.mongodbUsername() == null ||
+                    VoidCore.CONFIG.mongodbPassword() == null
+            ) {
+                return;
+            }
+            ServerPlayerEntity player = handler.player;
+            PlayerActionService service = new PlayerActionService(server.getPlayerManager());
+            service.playerJoin(player.getUuid());
+        });
+    }
+
+}
--- src/main/java/voideventhub/voidcore/common/events/VPSEvents.java
+++ src/main/java/voideventhub/voidcore/common/events/VPSEvents.java
@@ -7,16 +7,13 @@ public class VPSEvents {
 
     public static void register() {
         ServerTickEvent.register();
+        PlayerDisconnectEvent.register();
+        PlayerJoinEvent.register();
+        AfterDeathEvent.register();
 
-        // These events make queries to the database, so we only want to register them
-        // if we have a valid connection to avoid crashes
         if (((MongoDbRepository)MongoDbRepository.getInstance()).validConnection()) {
             VoidCore.LOGGER.error("Could not connect to database.");
-            return;
         }
-
-        EntityUnloadEvent.register();
-        EntityLoadEvent.register();
     }
 
 }
