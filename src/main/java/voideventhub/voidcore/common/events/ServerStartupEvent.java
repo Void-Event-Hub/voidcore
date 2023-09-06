ff8ed83e3494b8170d98032237edecb738010f31
DaanSchar <Daan.Schaer@Gmail.com>
2023-09-06 17:16:03 +0200

    chore: server will now close if it cannot connect to mongodb

---
+++ src/main/java/voideventhub/voidcore/common/events/ServerStartupEvent.java
@@ -0,0 +1,26 @@
+package voideventhub.voidcore.common.events;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import voideventhub.voidcore.common.VoidCore;
+import voideventhub.voidcore.repository.MongoDbRepository;
+
+public class ServerStartupEvent {
+
+    public static void register() {
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            boolean connectedToDatabase = ((MongoDbRepository) MongoDbRepository.getInstance()).validConnection();
+
+            if (server.isSingleplayer()) {
+                return;
+            }
+
+            if (connectedToDatabase) {
+                VoidCore.LOGGER.info("Successfully connected to database.");
+                return;
+            }
+            VoidCore.LOGGER.error("Could not connect to database. Check the config if the credentials are correct.");
+            throw new IllegalArgumentException("Could not connect to database");
+        });
+    }
+
+}
--- src/main/java/voideventhub/voidcore/common/events/VPSEvents.java
+++ src/main/java/voideventhub/voidcore/common/events/VPSEvents.java
@@ -1,19 +1,13 @@
 package voideventhub.voidcore.common.events;
 
-import voideventhub.voidcore.common.VoidCore;
-import voideventhub.voidcore.repository.MongoDbRepository;
-
 public class VPSEvents {
 
     public static void register() {
+        ServerStartupEvent.register();
         ServerTickEvent.register();
         PlayerDisconnectEvent.register();
         PlayerJoinEvent.register();
         AfterDeathEvent.register();
-
-        if (((MongoDbRepository)MongoDbRepository.getInstance()).validConnection()) {
-            VoidCore.LOGGER.error("Could not connect to database.");
-        }
     }
 
 }
